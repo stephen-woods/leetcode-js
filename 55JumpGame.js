71275e51e6215fb3dc7ba155e558a7ee93467217
Stephen Woods <stephen.woods@altarasystems.com>
2024-07-04 06:40:13 -0400

    Fixed jump game

--- 55JumpGame.js
+++ 55JumpGame.js
@@ -7,14 +7,12 @@
 //  
 //
 // Example 1:
-//
-// Input: nums = [2,3,1,1,4]
-// Output: true
+// Input: nums = [2,3,1,1,4] Output: true
 // Explanation: Jump 1 step from index 0 to 1, then 3 steps to the last index.
 // Example 2:
 //
 // Input: nums = [3,2,1,0,4]
-// Output: false
+// Output: fals
 // Explanation: You will always arrive at index 3 no matter what. Its maximum 
 // jump length is 0, which makes it impossible to reach the last index.
 //  
@@ -29,43 +27,17 @@
  * @return {boolean}
  */
 var canJump = function(nums) {
-  if (nums.length <= 1) {
-    return true;
-  }
-
-  let mj = nums[0];
-  for (let i = 0; i < nums.length - 1; i++) {
-    console.log(`${i}  ${nums[i]}`);
-    if (i > mj) {
-      return false;
+  var ret = true;
+  var maxDistance = 0;
+  for (let i = 0; i < nums.length; i++) {
+    if (i > maxDistance) {
+      ret = false;
+      break;
     }
-    
-    let jump = i + nums[i];
-    if (jump > mj) {
-      mj = jump
-    }
+    const distance = nums[i] + i;
+    maxDistance = Math.max(maxDistance, distance);
   }
-
-  return true;
-};
-
-function canJumpRec(nums, s) {
-  if (s >= nums.length - 1) {
-    return true;
-  }
-
-  let jumps = nums[s];
-
-  if (jumps === 0) {
-    return false;
-  }
-
-  for (let i = jumps; i > 0; i--) {
-    if (canJumpRec(nums, s + i)) {
-      return true;
-    }
-  }
-  return false; 
+  return ret;
 }
 
 let test1 = canJump([2,3,1,1,4]);
